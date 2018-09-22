84a8b2ac0dac26092acfe018b8bf84c5d16066d0
Gcenx <gcenx83@gmail.com>
2018-09-22 11:24:00 -0400

    Restore wineboot

--- programs/wineboot/wineboot.c
+++ programs/wineboot/wineboot.c
@@ -922,7 +922,6 @@ static INT_PTR CALLBACK wait_dlgproc( HWND hwnd, UINT msg, WPARAM wp, LPARAM lp 
     return 0;
 }
 
-/*
 static HWND show_wait_window(void)
 {
     const char *config_dir = wine_get_config_dir();
@@ -939,7 +938,6 @@ static HWND show_wait_window(void)
     HeapFree( GetProcessHeap(), 0, name );
     return hwnd;
 }
-*/
 
 static HANDLE start_rundll32( const char *inf_path, BOOL wow64 )
 {
@@ -1016,7 +1014,7 @@ static void update_wineprefix( BOOL force )
 
         if ((process = start_rundll32( inf_path, FALSE )))
         {
-/*            HWND hwnd = show_wait_window();*/
+            HWND hwnd = show_wait_window();
             for (;;)
             {
                 MSG msg;
@@ -1028,7 +1026,7 @@ static void update_wineprefix( BOOL force )
                 }
                 else while (PeekMessageW( &msg, 0, 0, 0, PM_REMOVE )) DispatchMessageW( &msg );
             }
-/*            DestroyWindow( hwnd );*/
+            DestroyWindow( hwnd );
         }
         WINE_MESSAGE( "wine: configuration in '%s' has been updated.\n", config_dir );
     }
